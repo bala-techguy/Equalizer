312b35d2e97a83011196ea40c7efa32bcd9b7e46
Bala <bala41288@gmail.com>
2018-12-11 22:08:08 +0530

    Functionality to View Subscriber details

--- src/app/components/crud-subscribers/crud-subscribers.component.ts
+++ src/app/components/crud-subscribers/crud-subscribers.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SubscriberService } from '../../services/subscriber.service'
 import { SubscriptionTypeService } from '../../services/subscription-type.service'
-import { ActivatedRoute } from '@angular/router';
+import { Subscriber } from '../../models/subscriber';
+import { ActivatedRoute, Router } from '@angular/router';
 import { typeSourceSpan } from '@angular/compiler';
 
 @Component({
@@ -15,7 +16,12 @@ export class CrudSubscribersComponent implements OnInit {
   types: string[];
   today: Date;
 
-  constructor(private subscriberService: SubscriberService, private route: ActivatedRoute, private subscriptionType: SubscriptionTypeService) { }
+  constructor(
+    private subscriberService: SubscriberService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private subscriptionType: SubscriptionTypeService
+  ) { }
   submitted: boolean;
   formControls = this.subscriberService.subscribersForm.controls;
 
@@ -23,29 +29,37 @@ export class CrudSubscribersComponent implements OnInit {
     this.action = this.route.snapshot.data['action'];
     var values = [];
     this.subscriptionType.getSubscriptionType().forEach(function (value) {
-      values.push(value.Name); 
+      values.push(value.Name);
     });
     this.types = values;
-    console.log (this.currentDate());
+    console.log(this.currentDate());
 
 
-    // if (this.action == 'Add'){
-    //   console.log(this.action);
-    //   this.subscriberService.subscribersForm.controls['CreatedDate'].setValue(this.currentDate(), {onlySelf: true});
-    // }
+    if (this.action == 'Add') {
+      console.log(this.action);
+      this.subscriberService.subscribersForm.controls['CreatedDate'].setValue(this.currentDate(), { onlySelf: true });
+    }
+    else {
+      
+    }
+    
 
   }
 
   currentDate() {
     const currentDate = new Date();
-    return currentDate.toISOString().substring(0,10);
+    return currentDate.toISOString().substring(0, 10);
   }
 
-  onSubmit() {
+  onSubmit(model: Subscriber, isValid: boolean) {
     this.submitted = true;
-    if (this.subscriberService.subscribersForm.valid) {
-      //if (this.subscriberService.subscribersForm.get('').value == null)
-      //insert
+    if (isValid) {
+      if (this.subscriberService.subscribersForm.get('InternalId').value == null) {
+        //console.log(model, isValid);
+        this.subscriberService.insertSubscribers(model);
+        this.router.navigate(['/subscribers']);
+      }
+
       this.submitted = false;
     }
 
